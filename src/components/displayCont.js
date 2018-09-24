de8df6b7a533d99efaec16d9408bbc479e22443d
Kevin Schmitz <kfschmitz@gmail.com>
2018-09-24 13:25:45 -0700

    adding enzyme tests

--- src/components/displayCont.js
+++ src/components/displayCont.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Content = (props) => {
   console.log(props)
 
-  if (!props.guesses.length) {
-    return <p>There are no guesses to show.</p>
-  }
+  // if (!props.guesses.length) {
+  //   return <p>There are no guesses to show.</p>
+  // }
 
   return (
     <div>
